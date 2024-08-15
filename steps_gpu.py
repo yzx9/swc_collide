53257e64dd7cea69f0eeaf2121c8f8d50dacadd5
yzx9 <yuan.zx@outlook.com>
2024-08-15 21:09:50 +0800

    refactor: reduce useless read operator

--- steps_gpu.py
+++ steps_gpu.py
@@ -1,8 +1,8 @@
-# type: ignore
+# pyright: reportInvalidTypeForm=false, reportArgumentType=false
 import argparse
+import os
 from typing import Optional
 
-import numpy.typing as npt
 import swcgeom
 import warp as wp
 from tqdm import tqdm
@@ -10,13 +10,16 @@ from tqdm import tqdm
 from resampler import IsometricResampler
 
 
-def main(
+def resolve(
     fname: str,
     *,
     output: Optional[str] = None,
     resample: bool = False,
+    gap: float = 0,
+    step: float = 0.01,
+    iterates: int = 10000,
+    device: str = "cpu",
     verbose: bool = False,
-    **kwargs,
 ):
     t = swcgeom.Tree.from_swc(fname)
     if resample:
@@ -27,88 +30,65 @@ def main(
     if verbose:
         print(f"n_nodes: {t.number_of_nodes()}")
 
-    new_xyz = run(t.xyz(), t.r(), verbose=verbose, **kwargs)
-    for i, name in enumerate([t.names.x, t.names.y, t.names.z]):
-        t.ndata[name] = new_xyz[..., i]
-
-    if output is not None:
-        dest = (
-            output
-            if not os.path.isdir(output)
-            else os.path.join(output, os.path.split(fname)[-1])
-        )
-        t.to_swc(dest)
-    else:
-        print(t.to_swc())
-
-
-def run(
-    xyz: npt.NDArray,
-    r: npt.NDArray,
-    *,
-    step: float = 0.01,
-    gap: float = 0,
-    iterates: int = 10000,
-    device: str = "cpu",
-    verbose: bool = False,
-) -> int:
-    d_target = r.reshape(-1, 1) + r.reshape(1, -1) + gap
-    weight = wp.ones(d_target.shape, dtype=wp.float32, device=device)
+    xyz, r = t.xyz(), t.r()
+    r += gap / 2  # add half gap to radius
 
     xyz = wp.from_numpy(xyz, dtype=wp.vec3f, device=device)
-    d_target = wp.from_numpy(d_target, dtype=wp.float32, device=device)
-
+    r = wp.from_numpy(r, dtype=wp.float32, device=device)
     for _ in tqdm(range(iterates)) if verbose else range(iterates):
         out = wp.zeros(r.shape[0], dtype=wp.vec3f, device=device)
         wp.launch(
-            run_step,
+            resolve_step,
             dim=r.shape,
-            inputs=[xyz, weight, d_target, step],
+            inputs=[xyz, r, step],
             outputs=[out],
             device=device,
         )
         xyz = out
 
-    return xyz.numpy()
+    xyz = xyz.numpy()
+    for i, name in enumerate([t.names.x, t.names.y, t.names.z]):
+        t.ndata[name] = xyz[..., i]
+
+    if output is None:
+        print(t.to_swc())
+    elif os.path.isdir(output):
+        t.to_swc(os.path.join(output, os.path.split(fname)[-1]))
+    else:
+        t.to_swc(output)
 
 
 @wp.kernel
-def run_step(
-    points: wp.array(dtype=wp.vec3f),
-    weight: wp.array(dtype=wp.float32, ndim=2),
-    d_target: wp.array(dtype=wp.float32, ndim=2),
+def resolve_step(
+    xyz: wp.array(dtype=wp.vec3f),
+    r: wp.array(dtype=wp.float32),
     step: wp.float32,
     out: wp.array(dtype=wp.vec3f),
 ):
     i = wp.tid()
-    direction = get_direction(points, i, weight, d_target)
-    out[i] = points[i] + step * direction
+    direction = get_weighted_direction(xyz, r, i)
+    out[i] = xyz[i] + step * direction
 
 
 @wp.func
-def get_direction(
-    points: wp.array(dtype=wp.vec3f),
+def get_weighted_direction(
+    xyz: wp.array(dtype=wp.vec3f),
+    r: wp.array(dtype=wp.float32),
     i: int,
-    weight: wp.array(dtype=wp.float32, ndim=2),
-    d_target: wp.array(dtype=wp.float32, ndim=2),
 ) -> wp.vec3f:
     vec = wp.vec3f(0.0, 0.0, 0.0)
-    for j in range(points.shape[0]):
+    for j in range(xyz.shape[0]):
         if i != j:
-            dis = distance(points[j], points[i])
-            if dis < d_target[j][i]:
-                vec += (points[i] - points[j]) * weight[i][j]
+            dis = wp.length(xyz[j] - xyz[i])
+            if dis < r[i] + r[j]:
+                weight = r[i] / (r[i] + r[j])
+                vec += (xyz[i] - xyz[j]) * weight
 
     l = wp.length(vec)
     if l > 0:
         vec /= l
 
     return vec
-
-
-@wp.func
-def distance(pa: wp.vec3f, pb: wp.vec3f) -> wp.float32:
-    return wp.length(pa - pb)
 
 
 if __name__ == "__main__":
@@ -123,7 +103,7 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--verbose", action="store_true")
 
     args = parser.parse_args()
-    main(
+    resolve(
         args.fname,
         output=args.output,
         gap=args.gap,
